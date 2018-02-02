0a4ccd1cbf5889e4d5cacef855df7ac7dc34b62a
DenisKaizer <1kaizerd@gmail.com>
2018-02-02 21:20:06 +0300

    closeLottery

--- main.sol
+++ main.sol
@@ -42,6 +42,18 @@ contract Ownable {
 
 }
 
+contract LotteryFactory is Ownable {
+
+  uint256 jackpot;
+  uint256 lotteryCounter;
+  address[] lotteries;
+  address token = 0x0;
+
+  function createLottery() onlyOwner {
+    lotteries.push(new Lottery(token, owner));
+  }
+}
+
 contract ERC20 {  // ERC20 interface
   uint public totalSupply;
 
@@ -63,7 +75,6 @@ contract Stateful {
   Init,
   sellIsActive,
   sellIsOver,
-  lotteryFinished,
   lotteryClosed
   }
   State public state = State.Init;
@@ -101,6 +112,8 @@ contract Lottery is Ownable, Stateful {
   ERC20 betToken;
   uint startLotteryBlock;
   uint stopLotteryBlock;
+  address factory;
+
   modifier onlyOwnerOrLotteryManager() {
     require(msg.sender == owner || msg.sender == lotteryManager);
     _;
@@ -112,11 +125,11 @@ contract Lottery is Ownable, Stateful {
   }
 
   modifier lotteryFinished() {
-    require(state == State.lotteryFinished);
+    require(state == State.sellIsOver);
     _;
   }
 
-  function Lottery(address _token,uint startLotteryBlock,uint stopLotteryBlock) {
+  function Lottery(address _token, address _owner,uint _startLotteryBlock,uint _stopLotteryBlock) {
     betToken = ERC20(_token);
     dataPrize[50] = 1000000; // 1/11,688,053.52
     dataPrize[41] = 50000; // 1/913,129.18
@@ -126,12 +139,17 @@ contract Lottery is Ownable, Stateful {
     dataPrize[21] = 7; // 1/701.33
     dataPrize[11] = 4; // 1/91.98
     dataPrize[1] = 4; // 1/38.32
+    dataPrize[0] = 0;
     dataPowerPlay[0] = 1;
     dataPowerPlay[1] = 2;
     dataPowerPlay[2] = 3;
     dataPowerPlay[3] = 4;
     dataPowerPlay[4] = 5;
     dataPowerPlay[5] = 10;
+    owner = owner;
+    startLotteryBlock = _startLotteryBlock;
+    stopLotteryBlock = _stopLotteryBlock;
+    factory = msg.sender;
   }
 
   function setManager(address _manager) onlyOwner {
@@ -153,6 +171,7 @@ contract Lottery is Ownable, Stateful {
     //uint tokenAmount = 2 * 1 ether;
     //betToken.transferFrom(msg.sender, this, tokenAmount);
     usersTickets[msg.sender].push(Ticket(wb1, wb2, wb3, wb4, wb5, rb, pp));
+    //jackpot += tokenAmount;
   }
 
   function random(uint8 upper) public returns (uint8 randomNumber) {
@@ -166,7 +185,6 @@ contract Lottery is Ownable, Stateful {
 
 
   function chooseWinnersTicket() onlyOwnerOrLotteryManager {
-      
     require(block.number > startLotteryBlock);
     winTicket.wb1 = random(69);
     winTicket.wb2 = random(69);
@@ -176,6 +194,7 @@ contract Lottery is Ownable, Stateful {
     winTicket.rb = random(26);
     setState(State.sellIsOver);
   }
+
   function refund() {
     require(block.number > stopLotteryBlock);
     betToken.transfer(msg.sender, 2 * 1 ether);
@@ -188,7 +207,7 @@ contract Lottery is Ownable, Stateful {
     Ticket _ticket;
     for (uint i = 0; i < usersTickets[player].length; i++) {
       _ticket = usersTickets[player][i];
-      uint8 wbcount;
+      uint8 wbCount;
       uint8 rb;
       if (_ticket.wb1 == winTicket.wb1) {
         wbcount++;
@@ -208,17 +227,32 @@ contract Lottery is Ownable, Stateful {
       if (_ticket.rb == winTicket.rb) {
         rb = 1;
       }
-      count += dataPrize[wbcount*10 + rb] * dataPowerPlay[_ticket.pp];
+      uint8 category = wbcount * 10 + rb;
+      if (category == 51) {
+        count += jackpot;
+      }
+      else {
+        count += dataPrize[category] * dataPowerPlay[_ticket.pp];
+      }
     }
     return count;
   }
+
+  event RewardRecieved(uint256);
 
   function getReward() {
     uint256 reward;
     reward = checkMyTicket(msg.sender);
     betToken.transfer(msg.sender, reward);
+    // delete from mapping
+    Reward(reward);
   }
 
-
+  function closeLottery() onlyCrowdsaleManagerOrOwner {
+    setState(State.lotteryClosed);
+    uint256 tokenAmount;
+    tokenAmount = betToken.balanceOf(this);
+    betToken.transfer(factory, tokenAmount);
+  }
 
 }
