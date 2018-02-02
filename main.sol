b9c3222b6e075b5fa03446379eb89f24170e66c9
DenisKaizer <1kaizerd@gmail.com>
2018-02-02 18:48:30 +0300

    chooseWinnersTicket() checkMyTicket() Statefull

--- main.sol
+++ main.sol
@@ -1,6 +1,46 @@
 pragma solidity ^0.4.15;
 
-import "Ownable.sol";
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
 
 contract ERC20 {  // ERC20 interface
   uint public totalSupply;
@@ -18,27 +58,60 @@ contract ERC20 {  // ERC20 interface
   event Transfer(address indexed from, address indexed to, uint value);
 }
 
+contract Stateful {
+  enum State {
+  Init,
+  sellIsActive,
+  sellIsOver,
+  lotteryFinished,
+  lotteryClosed
+  }
+  State public state = State.Init;
 
-contract Lottery is Ownable {
+  event StateChanged(State oldState, State newState);
+
+  function setState(State newState) internal {
+    State oldState = state;
+    state = newState;
+    StateChanged(oldState, newState);
+  }
+}
+
+contract Lottery is Ownable, Stateful {
 
   struct Ticket {
-    uint8 wb1;
-    uint8 wb2;
-    uint8 wb3;
-    uint8 wb4;
-    uint8 wb6;
-    uint8 red;
-    uint8 pp;
+  uint8 wb1;
+  uint8 wb2;
+  uint8 wb3;
+  uint8 wb4;
+  uint8 wb5;
+  uint8 rb;
+  uint8 pp;
   }
+
+  uint8 _seed = 0;
 
   mapping (uint8 => uint) dataPrize; // 50 => 1000000, 41 => 50000, ......
   mapping (address => uint8) ticketsNumber;
-  mapping (address => Ticket[]) usersTickets;
+  mapping (address => Ticket[]) public usersTickets;
+  mapping (uint8 => uint) public dataPowerPlay;
   uint256 jackpot;
   address lotteryManager;
+  Ticket winTicket;
+  ERC20 betToken;
 
   modifier onlyOwnerOrLotteryManager() {
     require(msg.sender == owner || msg.sender == lotteryManager);
+    _;
+  }
+
+  modifier sellIsActive() {
+    require(state == State.sellIsActive);
+    _;
+  }
+
+  modifier lotteryFinished() {
+    require(state == State.lotteryFinished);
     _;
   }
 
@@ -51,7 +124,7 @@ contract Lottery is Ownable {
     dataPrize[30] = 7; // 1/579.76
     dataPrize[21] = 7; // 1/701.33
     dataPrize[11] = 4; // 1/91.98
-    dataPrize[01] = 4; // 1/38.32
+    dataPrize[1] = 4; // 1/38.32
     dataPowerPlay[0] = 1;
     dataPowerPlay[1] = 2;
     dataPowerPlay[2] = 3;
@@ -61,29 +134,74 @@ contract Lottery is Ownable {
   }
 
   function setManager(address _manager) onlyOwner {
-
+    lotteryManager = _manager;
   }
 
   function buyTicket(
-    uint8 wb1,
-    uint8 wb2,
-    uint8 wb3,
-    uint8 wb4,
-    uint8 wb5,
-    uint8 rb,
-    uint8 pp)
+  uint8 wb1,
+  uint8 wb2,
+  uint8 wb3,
+  uint8 wb4,
+  uint8 wb5,
+  uint8 rb,
+  uint8 pp) sellIsActive
   {
     require((wb1 <= 69) && (wb2 <= 69) && (wb3 <= 69) && (wb4 <= 69) && (wb5 <= 69) && (rb <= 26));
-    require(betToken.allowance(msg.sender, this) >=  2 * 1 ether);
+    //require(betToken.allowance(msg.sender, this) >=  2 * 1 ether);
     require(usersTickets[msg.sender].length < 25);
-    uint tokenAmount = 2 * 1 ether;
-    require(betToken.transferFrom(msg.sender, this, tokenAmount));
-    msg.sender.transfer(tokenAmount);
+    //uint tokenAmount = 2 * 1 ether;
+    //betToken.transferFrom(msg.sender, this, tokenAmount);
     usersTickets[msg.sender].push(Ticket(wb1, wb2, wb3, wb4, wb5, rb, pp));
   }
 
-
-  function winnersTicket() onlyOwnerOrLotteryManager {
-
+  function random(uint8 upper) public returns (uint8 randomNumber) {
+    _seed = uint8(sha3(sha3(block.blockhash(block.number), _seed), now));
+    return _seed % upper;
   }
+
+  function startSale() onlyOwnerOrLotteryManager {
+    setState(State.sellIsActive);
+  }
+
+
+  function chooseWinnersTicket() onlyOwnerOrLotteryManager {
+    winTicket.wb1 = random(69);
+    winTicket.wb2 = random(69);
+    winTicket.wb3 = random(69);
+    winTicket.wb4 = random(69);
+    winTicket.wb5 = random(69);
+    winTicket.rb = random(26);
+    setState(State.sellIsOver);
+  }
+
+  function checkMyTicket() lotteryFinished view returns(uint256) {
+    uint256 count;
+    Ticket _ticket;
+    for (uint i = 0; i < usersTickets[msg.sender].length; i++) {
+      _ticket = usersTickets[msg.sender][i];
+      uint8 wbcount;
+      uint8 rb;
+      if (_ticket.wb1 == winTicket.wb1) {
+        wbcount++;
+      }
+      if (_ticket.wb2 == winTicket.wb1) {
+        wbcount++;
+      }
+      if (_ticket.wb3 == winTicket.wb1) {
+        wbcount++;
+      }
+      if (_ticket.wb4 == winTicket.wb1) {
+        wbcount++;
+      }
+      if (_ticket.wb5 == winTicket.wb1) {
+        wbcount++;
+      }
+      if (_ticket.rb == winTicket.rb) {
+        rb = 1;
+      }
+      count += dataPrize[wbcount*10 + rb];
+    }
+    return count;
+  }
+
 }
